b6f2e555f8f431825c92ce459c2669bb17fc0df1
Tom Vincent <tlvince@gmail.com>
2011-11-16 16:59:54 +0000

    Move defaults into parseargs

--- flashback.py
+++ flashback.py
@@ -10,15 +10,26 @@ import os.path
 import subprocess
 import argparse
 
-def parse_arguments(log_levels):
+def parse_arguments():
     """Parse the command-line arguments."""
+    # Defaults
+    log_levels = [k for k in logging.__dict__["_levelNames"].keys() if not
+            isinstance(k, int)]
+    mplayer = "mplayer -fs"
+    quvi = ["quvi", "--format=best", "--exec"]
+    quvi.append("{0} %u".format(mplayer))
+
     parser = argparse.ArgumentParser(description=__doc__.split("\n")[0])
     parser.add_argument("-l", "--log-level", default="WARN", choices=log_levels,
-            help="logging level (default: %(default)s)")
+       help="logging level (default: %(default)s)")
     parser.add_argument("-f", "--log-file", help="logging file name")
+    parser.add_argument("-m", "--media-player", default=mplayer,
+        help="media player command (default: '%(default)s')")
+    parser.add_argument("-p", "--parser", default=quvi,
+        help="video hosting URL parser command (default: %(default)s)")
     return parser.parse_args()
 
-def handles():
+def quvi_hosts():
     """Format quvi's supported hosts."""
     # Get all hosts
     hosts = subprocess.check_output(["quvi", "--support"]).split("\n")[:-1]
@@ -36,6 +47,8 @@ def handles():
 
 def handler(host, path, known_hosts, parser):
     """URL handler."""
+    url = "http://{0}{1}".format(host, path)
+    logging.debug("handling {0}".format(url))
     # Ignore root paths
     if len(path) == 1:
         return
@@ -43,28 +56,20 @@ def handler(host, path, known_hosts, parser):
     if host.startswith("www."):
         host = host[4:]
     if host in known_hosts:
-        url = "http://{0}{1}".format(host, path)
         cmd = parser + [url]
-        logging.info("loading {0}".format(url))
+        logging.info("playing {0}".format(url))
         subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def main():
     """Start execution of flashback."""
-    log_levels = [k for k in logging.__dict__["_levelNames"].keys() if not
-            isinstance(k, int)]
+    args = parse_arguments()
 
-    args = parse_arguments(log_levels)
-
+    # Setup logging
     logging.basicConfig(level=args.log_level, filename=args.log_file,
         format="{name}: %(levelname)s: %(message)s".format(
             name=os.path.basename(__file__)))
 
-    mplayer = "mplayer -fs -really-quiet"
-    quvi = "quvi --quiet --format=best --exec"
-    parser = quvi.split(" ")
-    parser.append("{0} %u".format(mplayer))
-
-    known_hosts = handles()
+    known_hosts = quvi_hosts()
 
     pattern = re.compile("GET (.*) HTTP.*\nHost: ([^\r\n]*)")
     try:
@@ -73,7 +78,7 @@ def main():
         for timestamp, packet in pc:
             regex = pattern.search(packet)
             if regex:
-                handler(regex.group(2), regex.group(1), known_hosts, parser)
+                handler(regex.group(2), regex.group(1), known_hosts, args.parser)
     except OSError:
         logging.error("must be run as root")
     except KeyboardInterrupt:
